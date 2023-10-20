a87aab9ae51539902c725db608eedef3c3f90a32
Shantanu <shantanusoman01@gmail.com>
2023-10-20 11:18:21 +0000

    LayerArch

---
+++ BzLayer/Class1.cs
@@ -0,0 +1,11 @@
+﻿using Entity;
+using DataLayer;
+namespace BzLayer;
+public class MovieBZ
+{   
+    public List<Movie> GetMovies(){
+        DataAccess dataAccess=new DataAccess();
+        return dataAccess.GetMovies();
+    }
+
+}
---
+++ Hello/Program.cs
@@ -0,0 +1,23 @@
+﻿using Entity;
+using BzLayer;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        MovieBZ bz=new MovieBZ();
+        List<Movie> movies=bz.GetMovies();
+        if(movies!=null){
+            foreach(var m in movies){
+                Console.WriteLine($"{m.Id} {m.Name} {m.Rating} {m.Ryear}");
+
+            }
+        }
+        else{
+            System.Console.WriteLine("No Movies Present");
+        }
+        
+        
+        
+    }
+}
